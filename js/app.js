a8f68341b9ae84b81ddc2825b6232b035a712296
Michael <mamorton1092@gmail.com>
2016-06-03 18:19:02 -0400

    Almost done

--- js/app.js
+++ js/app.js
@@ -15,56 +15,123 @@ app.config(['$routeProvider', function ($routeProvider) {
 
 
 app.controller('HeroListController', ['$scope', '$http', 'HeroService', function($scope, $http, HeroService) {
+   
+    for (var i = 0; i < 100; i++){
+        if (i % 100 === 0){
     $http({
         method : "GET",
-        url : "http://gateway.marvel.com:80/v1/public/characters?apikey=aecaf7c9b04cdd88b0fa5903dbe6402e&limit=100"
+        url : "http://gateway.marvel.com:80/v1/public/characters?apikey=bc9b3cb24d6a0dc5cb796ee19906f858&limit=100&hash=6e9b6cd1099b19df878caf48f374cbb8&ts=1"
     }).then(function mySucces(response) {
-        var heroData = response.data;
-        console.log(heroData)
+        var heroData = response.data.data.results;
+        HeroService.addHeroes(heroData);
+        $scope.heroes = HeroService.allHeroes();
+//        console.log(heroData); 
+    }, function myError(response) {
+        console.log(response.statusText);
+    });
+    
+    $scope.heroes = HeroService.allHeroes();
+//    console.log('youve reached the hero list');
+            
+    $scope.selectHero = function (hero) {
+        HeroService.addToCart(hero);
+    };
+        }
+
+    }
+}]);
+
+app.controller('HeroSelectController', ['$scope', '$http', 'HeroService', function($scope, $http, HeroService) {
+//    console.log('Hero Selector');
+    
+        for (var j = 0; j < 99; j++){
+        if (j % 100 === 0){
+    $http({
+        method : "GET",
+        url : "http://gateway.marvel.com:80/v1/public/characters?apikey=bc9b3cb24d6a0dc5cb796ee19906f858&limit=5&hash=6e9b6cd1099b19df878caf48f374cbb8&ts=1"
+    }).then(function mySucces(response) {
+        var heroData = response.data.data.results;
+        HeroService.addHeroes(heroData);
+        $scope.heroes = HeroService.allHeroes();
+        console.log(heroData); 
+        for(var k=0;k<1/*heroData.length*/;k++) {
+            console.log(heroData[k].events.collectionURI+"?apikey=bc9b3cb24d6a0dc5cb796ee19906f858&hash=6e9b6cd1099b19df878caf48f374cbb8&ts=1");
+            (function(k){$http({
+                method :"GET",
+                url : heroData[k].events.collectionURI+"?apikey=bc9b3cb24d6a0dc5cb796ee19906f858&hash=6e9b6cd1099b19df878caf48f374cbb8&ts=1"}).then(
+                function(res) {
+                    for (var l = 0; l <  res.data.data.results.length; l++){
+                        (function(l, res){$http({
+                                method : "GET",
+                                url : res.data.data.results[l].characters.collectionURI+"?apikey=bc9b3cb24d6a0dc5cb796ee19906f858&hash=6e9b6cd1099b19df878caf48f374cbb8&ts=1"
+                            }).then(function mySucces(test) {
+                            res.data.data.results[l].heroes = test.data.data.results
+//                   console.log(heroData); 
+                   HeroService.addEvents(k, res.data.data);
+                    $scope.heroes = HeroService.allHeroes();
+                    console.log(res)
+    }, function myError(response) {
+        console.log(response.statusText);
+    });})(l, res)}
+    
+    })})(k)
+    }
     }, function myError(response) {
         console.log(response.statusText);
     });
     
     $scope.heroes = HeroService.allHeroes();
     console.log('youve reached the hero list');
-    
+            
     $scope.selectHero = function (hero) {
         HeroService.addToCart(hero);
     };
+        }
+    }
+    $scope.eventDes = function (obj) {
+        localStorage['event'] = JSON.stringify(obj);
+        location.hash = "events";
+    }
+    
 }]);
 
-app.controller('HeroSelectController', ['$scope', 'HeroService', function($scope, HeroService) {
-    console.log('Hero Selector');
-}]);
+app.controller('HeroEventController', ['$scope','$http', 'HeroService', function($scope, $http , HeroService) {
+//    console.log('Hero Collection');
+    
+    $scope.event = JSON.parse(localStorage['event']);
 
-app.controller('HeroEventController', ['$scope', 'HeroService', function($scope, HeroService) {
-    console.log('Hero Collection');
+    $http({
+        method : "GET",
+        url : $scope.event.characters.collectionURI+"?apikey=bc9b3cb24d6a0dc5cb796ee19906f858&hash=6e9b6cd1099b19df878caf48f374cbb8&ts=1"
+    }).then(function mySucces(response) {
+        var heroData = response.data.data.results;
+        $scope.heroes = heroData
+//        console.log(heroData); 
+    }, function myError(response) {
+        console.log(response.statusText);
+    });
+    
+    
 }]);
 
 app.factory('HeroService', function() {
-    var heroes = [
-        { name: null, 
-         picture: null,
-         events: null,
-         all: null,
-        },
-    ];
-    
-    var collection = [];
+    var heroes = []; 
+    var events = [];
     
     return {
         allHeroes: function() {
             return heroes;
         },
-        selectHero: function (hero) {
-            collection.push(heroes.name);
+        addHeroes: function (hero) {
+            heroes = heroes.concat(hero);
         },
-        deselectHero: function (hero) {
-            collection.pop(heroes.name);
+        removeHeroes: function (hero) {
+            heroes.pop(hero);
         },
-        deselectHeroes: function() {
-            collection = [];
-        },
+        addEvents: function(l, events) {
+            console.log(l);
+            heroes[l].eventss = events;
+        }
     };
 });
 
